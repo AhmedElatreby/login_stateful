b6319301c2aeca09e9175f9ad4b0ca4e6b52df2e
AhmedElatreby <ahmedsport@hotmail.com>
2023-03-29 23:16:31 +0100

    fc

---
+++ lib/src/screens/login_screen.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  createState() {
+    return LoginScreenState();
+  }
+}
+
+class LoginScreenState extends State<LoginScreen> {
+  @override
+  Widget build(context) {
+    return Container(
+      margin: const EdgeInsets.all(20.0),
+      child: Form(
+        child: Column(
+          children: [
+            emailField(),
+            passwordField(),
+            Container(
+              margin: EdgeInsets.only(top: 25.0),
+            ),
+            submitButton(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget emailField() {
+    return const TextField(
+      keyboardType: TextInputType.emailAddress,
+      decoration: InputDecoration(
+        icon: Icon(Icons.email),
+        labelText: 'Email Address',
+        hintText: 'you@example.com',
+      ),
+    );
+  }
+
+  Widget passwordField() {
+    return const TextField(
+      obscureText: true,
+      decoration: InputDecoration(
+        icon: Icon(Icons.password),
+        labelText: 'Password',
+        hintText: 'Password',
+      ),
+    );
+  }
+
+  Widget submitButton() {
+    return ElevatedButton(
+      style: ElevatedButton.styleFrom(
+        padding: EdgeInsets.all(10.0),
+        primary: Colors.lightBlue,
+        onPrimary: Colors.black45,
+      ),
+      onPressed: () {},
+      child: Text('Submit!'),
+    );
+  }
+}
